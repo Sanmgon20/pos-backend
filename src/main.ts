1e078a3b6853be4858f06e2276f9a92b59b167df
Santiago Gonzalez <sanmgon02@gmail.com>
2026-09-06 19:56:40 -0300

    habilitar cors

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,8 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Pipe global de validación
+  app.enableCors();
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
